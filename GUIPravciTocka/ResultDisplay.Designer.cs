c558065b0f31a4ace088727701d0df58154c55ef
Ivan Spanja <IvanSp@dspace.hr>
2022-01-01 23:01:57 +0100

    Preinake

--- GUIPravciTocka/ResultDisplay.Designer.cs
+++ GUIPravciTocka/ResultDisplay.Designer.cs
@@ -33,46 +33,60 @@ namespace VsiteCSharpLinearnaJednadzbaGui
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDowna1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownb1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDowna2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownb2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNultocka1 = new System.Windows.Forms.TextBox();
+            this.textBox2Nultocka2 = new System.Windows.Forms.TextBox();
+            this.textBox3Sjeciste = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowna1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowna2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownb2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDownb1);
+            this.groupBox1.Controls.Add(this.numericUpDowna1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(25, 25);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(275, 159);
+            this.groupBox1.Size = new System.Drawing.Size(275, 134);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Coeficients";
+            this.groupBox1.Text = "Coeficients1";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.numericUpDownb2);
+            this.groupBox2.Controls.Add(this.numericUpDowna2);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(25, 206);
+            this.groupBox2.Location = new System.Drawing.Point(25, 188);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(275, 172);
+            this.groupBox2.Size = new System.Drawing.Size(275, 145);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Coeficients";
+            this.groupBox2.Text = "Coeficients2";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.Size = new System.Drawing.Size(28, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "&a:";
+            this.label1.Text = "&a1:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -80,51 +94,166 @@ namespace VsiteCSharpLinearnaJednadzbaGui
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 75);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.Size = new System.Drawing.Size(28, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "&b:";
+            this.label2.Text = "&b1:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 119);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(19, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "&c:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(6, 40);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 17);
+            this.label4.Size = new System.Drawing.Size(28, 17);
             this.label4.TabIndex = 1;
-            this.label4.Text = "&a:";
+            this.label4.Text = "&a2:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(6, 87);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(20, 17);
+            this.label5.Size = new System.Drawing.Size(28, 17);
             this.label5.TabIndex = 2;
-            this.label5.Text = "&b:";
+            this.label5.Text = "&b2:";
+            // 
+            // numericUpDowna1
+            // 
+            this.numericUpDowna1.DecimalPlaces = 2;
+            this.numericUpDowna1.Location = new System.Drawing.Point(106, 33);
+            this.numericUpDowna1.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.numericUpDowna1.Minimum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDowna1.Name = "numericUpDowna1";
+            this.numericUpDowna1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDowna1.TabIndex = 3;
+            this.numericUpDowna1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numericUpDownb1
+            // 
+            this.numericUpDownb1.DecimalPlaces = 2;
+            this.numericUpDownb1.Location = new System.Drawing.Point(106, 73);
+            this.numericUpDownb1.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.numericUpDownb1.Minimum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownb1.Name = "numericUpDownb1";
+            this.numericUpDownb1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownb1.TabIndex = 4;
+            this.numericUpDownb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numericUpDowna2
+            // 
+            this.numericUpDowna2.DecimalPlaces = 2;
+            this.numericUpDowna2.Location = new System.Drawing.Point(106, 40);
+            this.numericUpDowna2.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.numericUpDowna2.Minimum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDowna2.Name = "numericUpDowna2";
+            this.numericUpDowna2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDowna2.TabIndex = 4;
+            this.numericUpDowna2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numericUpDownb2
+            // 
+            this.numericUpDownb2.DecimalPlaces = 2;
+            this.numericUpDownb2.Location = new System.Drawing.Point(106, 85);
+            this.numericUpDownb2.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.numericUpDownb2.Minimum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownb2.Name = "numericUpDownb2";
+            this.numericUpDownb2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownb2.TabIndex = 5;
+            this.numericUpDownb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "&Nultocka1:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 130);
+            this.label6.Location = new System.Drawing.Point(22, 410);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(19, 17);
+            this.label6.Size = new System.Drawing.Size(75, 17);
             this.label6.TabIndex = 3;
-            this.label6.Text = "&c:";
+            this.label6.Text = "&Nultocka2:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 464);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "&Sjeciste:";
+            // 
+            // textBoxNultocka1
+            // 
+            this.textBoxNultocka1.Location = new System.Drawing.Point(131, 359);
+            this.textBoxNultocka1.Name = "textBoxNultocka1";
+            this.textBoxNultocka1.ReadOnly = true;
+            this.textBoxNultocka1.Size = new System.Drawing.Size(120, 22);
+            this.textBoxNultocka1.TabIndex = 5;
+            // 
+            // textBox2Nultocka2
+            // 
+            this.textBox2Nultocka2.Location = new System.Drawing.Point(131, 410);
+            this.textBox2Nultocka2.Name = "textBox2Nultocka2";
+            this.textBox2Nultocka2.ReadOnly = true;
+            this.textBox2Nultocka2.Size = new System.Drawing.Size(120, 22);
+            this.textBox2Nultocka2.TabIndex = 6;
+            // 
+            // textBox3Sjeciste
+            // 
+            this.textBox3Sjeciste.Location = new System.Drawing.Point(131, 464);
+            this.textBox3Sjeciste.Name = "textBox3Sjeciste";
+            this.textBox3Sjeciste.ReadOnly = true;
+            this.textBox3Sjeciste.Size = new System.Drawing.Size(120, 22);
+            this.textBox3Sjeciste.TabIndex = 7;
             // 
             // ResultDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox3Sjeciste);
+            this.Controls.Add(this.textBox2Nultocka2);
+            this.Controls.Add(this.textBoxNultocka1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "ResultDisplay";
@@ -133,19 +262,32 @@ namespace VsiteCSharpLinearnaJednadzbaGui
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowna1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowna2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownb2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownb1;
+        private System.Windows.Forms.NumericUpDown numericUpDowna1;
+        private System.Windows.Forms.NumericUpDown numericUpDownb2;
+        private System.Windows.Forms.NumericUpDown numericUpDowna2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNultocka1;
+        private System.Windows.Forms.TextBox textBox2Nultocka2;
+        private System.Windows.Forms.TextBox textBox3Sjeciste;
     }
 }
