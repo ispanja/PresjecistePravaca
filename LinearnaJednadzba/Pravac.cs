cb1e1c83c4efca4060a39f4256ed4b32f6b00807
Ivan Spanja <IvanSp@dspace.hr>
2022-01-01 17:29:50 +0100

    Add project files.

---
+++ LinearnaJednadzba/Pravac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearnaJednadzba
+{
+    class Pravac
+    {
+        // y = ax + b
+        // a - koeficijent smjera; b - odsječak na ordinati
+        public double koefSmjera;
+        public double odsjecakNaY;
+
+        //Konstruktor
+        public Pravac(double koeficijentSmjera, double odsjecakOrdinata)
+        {
+            this.koefSmjera = koeficijentSmjera;
+            this.odsjecakNaY = odsjecakOrdinata;
+        }
+
+        public override string ToString()
+        {
+            return $"({koefSmjera}, {odsjecakNaY})";
+        }
+
+    }
+}
---
+++ LinearnaJednadzba/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearnaJednadzba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pravac pk = new Pravac(2, 4);
+            PravacStruct ps = new PravacStruct(-1, 2);
+
+            Pravac pk2 = pk;
+            PravacStruct ps2 = ps;
+
+            Console.WriteLine($"pk={pk}");
+            Console.WriteLine($"pk2={pk2}");
+            Console.WriteLine($"ps={ps}");
+            Console.WriteLine($"ps2={ps2}");
+
+            pk2.koefSmjera = 25;
+            ps2.odsjecakNaY = 58;
+
+            Console.WriteLine($"pk={pk}");
+            Console.WriteLine($"pk2={pk2}");
+            Console.WriteLine($"ps={ps}");
+            Console.WriteLine($"ps2={ps2}");
+
+
+
+            Console.ReadKey(false);
+        }
+    }
+}
